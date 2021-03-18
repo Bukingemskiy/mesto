22fd6ed2485de06abf152461730578c511a6d470
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-03-18 15:24:07 +0300

    написать README

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let popup = document.querySelector('.popup');
-let editPopupBtn = document.getElementById('popup_open');
+let editPopupBtn = document.querySelector('.profile__edit-button');
 let closePopupBtn = document.querySelector('.popup__close-button');
 let savePopupBtn = document.querySelector('.popup__save-button');
 let PopupName = document.getElementById('popup_name');
@@ -9,9 +9,16 @@ let subtitle = document.querySelector('.profile__subtitle');
 
 function openPopup() {
     popup.classList.add('popup_visible');
+    document.addEventListener("keyup", function(event) {
+        if (event.key === "Enter" && popup.classList.contains('popup_visible')) {
+          savePopupBtn.click();
+        }
+      });
 }
 
 function closePopup() {
+    PopupName.value=title.textContent;
+    PopupText.value=subtitle.textContent;
     popup.classList.remove('popup_visible');
 }
 
@@ -23,10 +30,9 @@ function editPopup() {
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let PopupEdit = document.querySelector('.popup__edit');
-    let inputs = PopupEdit.querySelectorAll('input');
-    title.textContent = inputs[0].value;
-    subtitle.textContent = inputs[1].value;
+    title.textContent = PopupName.value;
+    subtitle.textContent = PopupText.value;
+    closePopup();
 }
 
 editPopupBtn.addEventListener('click', editPopup);
