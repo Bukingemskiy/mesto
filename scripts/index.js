46dc130c4863aa197508260436cc9752a1a9c3e3
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-03-19 20:08:16 +0300

    hotfix bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,29 @@
 let popup = document.querySelector('.popup');
 let editPopupBtn = document.querySelector('.profile__edit-button');
 let closePopupBtn = document.querySelector('.popup__close-button');
-let PopupName = document.getElementById('popup_name');
-let PopupText = document.getElementById('popup_text');
+let popupName = document.getElementById('popup_name');
+let popupText = document.getElementById('popup_text');
 let title = document.querySelector('.profile__title');
 let subtitle = document.querySelector('.profile__subtitle');
-let PopupEdit = document.querySelector('.popup__edit');
+let popupEdit = document.querySelector('.popup__edit');
 
 function openPopup() {
+    popupName.value=title.textContent;
+    popupText.value=subtitle.textContent;
     popup.classList.add('popup_visible');
-    document.addEventListener("keyup", function(event) {
-        if (event.key === "Enter" && popup.classList.contains('popup_visible')) {
-          PopupEdit.submit();
-        }
-      });
 }
 
 function closePopup() {
-    PopupName.value=title.textContent;
-    PopupText.value=subtitle.textContent;
     popup.classList.remove('popup_visible');
-}
-
-function editPopup() {
-    PopupName.setAttribute('value', title.textContent);
-    PopupText.setAttribute('value', subtitle.textContent);
-    openPopup();
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    title.textContent = PopupName.value;
-    subtitle.textContent = PopupText.value;
+    title.textContent = popupName.value;
+    subtitle.textContent = popupText.value;
     closePopup();
 }
 
-editPopupBtn.addEventListener('click', editPopup);
+editPopupBtn.addEventListener('click', openPopup);
 closePopupBtn.addEventListener('click', closePopup);
-PopupEdit.addEventListener('submit', formSubmitHandler);
+popupEdit.addEventListener('submit', formSubmitHandler);
